41234848de15fc14bb793c1ec970f9f48e8d5748
Sven Sabas <ssabas@gmail.com>
2017-12-28 15:09:10 +0200

    Created command line entry point.

---
+++ adventure/command.py
@@ -0,0 +1,35 @@
+from adventure.dungeon import Dungeon
+from argparse import ArgumentParser
+import yaml
+
+def parse_args():
+    parser = ArgumentParser(description="Automatic hunt for the treasure. "
+                                        "Runs the script multiple times and "
+                                        "outputs the probability of reaching the "
+                                        "treasure.")
+    parser.add_argument('--file', type=str,
+                        help='''YAML file to load dungeon structure.''')
+    parser.add_argument('--samples', default=500, type=int,
+                        help='number of samples to be drawn. Must be an integer.')
+    arguments = parser.parse_args()
+    cfgdata = {"from_file": arguments.file,
+               "sample_size": arguments.samples}
+    return cfgdata
+
+
+def load_config(config_filename):
+    cfgfile = open(config_filename, 'r')
+    cfgdata = yaml.load(cfgfile)
+    cfgfile.close()
+    return cfgdata
+
+
+def run_dungeon(cfgdata):
+    dungeon_structure = load_config(cfgdata["from_file"])
+    test_dungeon = Dungeon(dungeon_structure)
+    return print(test_dungeon.success_chance(trials = cfgdata["sample_size"]))
+
+
+if __name__ == "__main__":
+    cfgdata = parse_args()
+    run_dungeon(cfgdata)
